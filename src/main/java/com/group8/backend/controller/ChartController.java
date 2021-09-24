f689d7976d2d89461e492d8d97a77963e80b589e
caposerenity <1464390895@qq.com>
2021-09-25 00:14:29 +0800

    ping test

---
+++ src/main/java/com/group8/backend/controller/ChartController.java
@@ -0,0 +1,21 @@
+package com.group8.backend.controller;
+
+import com.group8.backend.VO.ResponseVO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/chart")
+public class ChartController {
+    @GetMapping("/hello/")
+    public ResponseVO findChart(@PathVariable int id){
+
+        return ResponseVO.buildSuccess("hello");
+    }
+
+}
